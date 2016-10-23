ea52a5ab091791d0237b3eb39d6ab13ac96d696e
Chris Price <chris@puppetlabs.com>
2016-10-23 10:36:11 -0700

    Improve gatling launch script to manage node process

--- gatling/run_tests.sh
+++ gatling/run_tests.sh
@@ -1,8 +1,38 @@
 #!/usr/bin/env bash
 
+if [ -z "$COUCHBASE_HOST" ]; then
+    echo "Missing required environment variable 'COUCHBASE_HOST'!"
+    exit 1
+fi
+
 set -x
 set -e
 
+start_node()
+{
+    pushd ../nodejs
+    node src/server.js > >(./node_modules/.bin/bunyan) &
+    nodejs_pid=$!
+    popd
+}
+
+flush_couch()
+{
+    pushd ../nodejs
+    npm run couchbase-flush
+    popd
+}
+
+stop_node()
+{
+    kill $nodejs_pid
+}
+
+
+start_node
+flush_couch
 sbt "gatling:testOnly nodejs.NodeJSWriteSimulation"
 sbt "gatling:testOnly nodejs.NodeJSReadSimulation"
+flush_couch
 sbt "gatling:testOnly nodejs.NodeJSReadWriteSimulation"
+stop_node
--- gatling/src/test/scala/nodejs/Config.scala
+++ gatling/src/test/scala/nodejs/Config.scala
@@ -1,6 +1,6 @@
 package nodejs
 
 object Config {
-  val NUM_USERS = 10
+  val NUM_USERS = 200
   val NUM_REPETITIONS = 10
 }
